eb0fe3a8443f009fe05d528105eb4537071358d6
facchiR <roberta.facchi.88@gmail.com>
2019-11-29 17:15:38 +0100

    old version

--- 2-bowser.js
+++ 2-bowser.js
@@ -1,7 +1,6 @@
 var writeb = function(browser){
 	var bowser = document.getElementById("bowser"); 
 	bowser.innerHTML = browser; 
-	
 }
 window.addEventListener("load", function() {
   // PARSE USER AGENT
@@ -21,8 +20,8 @@ window.addEventListener("load", function() {
   console.log(result.platform.type);
   
   
-  localStorage.setItem('browserN', result.browser.name); 
+  
   // BROWSER INFO
-  var res = result.browser.name //+ "<br>" + result.browser.version + "<br>" + result.engine.name + "<br>" + result.os.name + "<br>" + result.os.version+ "<br>" + result.os.versionName + "<br>" + result.platform.type;
+  var res = result.browser.name + "<br>" + result.browser.version + "<br>" + result.engine.name + "<br>" + result.os.name + "<br>" + result.os.version+ "<br>" + result.os.versionName + "<br>" + result.platform.type;
   writeb(res);
 });

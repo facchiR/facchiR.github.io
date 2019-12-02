2681a204bdad8e4ca94f59c8fdddc115ecf19098
facchiR <roberta.facchi.88@gmail.com>
2019-12-02 14:17:49 +0100

    moving browser switch

--- 2-bowser.js
+++ 2-bowser.js
@@ -3,26 +3,41 @@ var writeb = function(browser){
 	bowser.innerHTML = browser; 
 }
 window.addEventListener("load", function() {
-  // PARSE USER AGENT
-  var result = bowser.getParser(navigator.userAgent).getResult();
+	
+	// PARSE USER AGENT
+	var result = bowser.getParser(navigator.userAgent).getResult();
   
-   // BROWSER INFO
-  console.log(result.browser.name);
-  localStorage.setItem("browser", result.browser.name); 
-  console.log(result.browser.version);
-  console.log(result.engine);
+	// BROWSER INFO
+	console.log(result.browser.name);
+	localStorage.setItem("browser", result.browser.name); 
+	console.log(result.browser.version);
+	console.log(result.engine);
 
-  // OPERATING SYSTEM
-  console.log(result.os.name);
-  console.log(result.os.version);
-  console.log(result.os.versionName);
+	// OPERATING SYSTEM
+	console.log(result.os.name);
+	console.log(result.os.version);
+	console.log(result.os.versionName);
 
-  // PLATFORM
-  console.log(result.platform.type);
+	// PLATFORM
+	console.log(result.platform.type);
   
   
   
-  // BROWSER INFO
-  var res = result.browser.name + "<br>" + result.browser.version + "<br>" + result.engine.name + "<br>" + result.os.name + "<br>" + result.os.version+ "<br>" + result.os.versionName + "<br>" + result.platform.type;
-  writeb(res);
+    // BROWSER INFO
+    var res = result.browser.name + "<br>" + result.browser.version + "<br>" + result.engine.name + "<br>" + result.os.name + "<br>" + result.os.version+ "<br>" + result.os.versionName + "<br>" + result.platform.type;
+    writeb(res);
+  
+    alert(localStorage.browser); 	
+	switch(localStorage.browser) {
+		case "Chrome":
+			console.log("CHECK 1.5.0")
+			// code block
+			break;
+		case "Samsung Internet for Android":
+			console.log("CHECK 1.5.1")
+			// code block
+			//break;
+		default:
+			// code block
+	}
 });
